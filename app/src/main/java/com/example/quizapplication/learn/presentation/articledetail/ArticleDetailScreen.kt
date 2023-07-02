b599756ef3ecfa3c2accf2b8c31c2734475f0794
Venkat-juju <ramanan1029195@gmail.com>
2023-07-02 22:09:32 +0530

    article detial webview added.

--- app/src/main/java/com/example/quizapplication/learn/presentation/articledetail/ArticleDetailScreen.kt
+++ app/src/main/java/com/example/quizapplication/learn/presentation/articledetail/ArticleDetailScreen.kt
@@ -1,6 +1,10 @@
 package com.example.quizapplication.learn.presentation.articledetail
 
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -15,6 +19,8 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -47,8 +53,33 @@ fun ArticleDetailScreen(
             )
         }
     ) { innerPadding ->
-        Column(modifier = Modifier.padding(innerPadding)) {
-            Text(text = "Article Detail Screen")
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            ArticleDetail()
         }
     }
+}
+
+@Composable
+fun ArticleDetail() {
+    AndroidView(
+        factory = {
+            WebView(it).apply {
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                )
+                webViewClient = WebViewClient()
+                loadUrl("file:///android_asset/about2.html")
+            }
+        },
+        update = {
+            it.loadUrl("file:///android_asset/about2.html")
+        },
+        modifier = Modifier
+            .padding(horizontal = 12.dp)
+    )
 }
